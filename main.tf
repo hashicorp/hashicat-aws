7ea98a157b2fd8a16ec91c81d2a5b6ae5de47907
Sean Carolan <scarolan@hashicorp.com>
2019-12-05 13:58:05 -0600

    Enable public DNS

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource aws_vpc "hashicat" {
   cidr_block           = var.address_space
-  enable_dns_hostnames = false
+  enable_dns_hostnames = true
 
   tags = {
     Name = "${var.prefix}-vpc"
