e22858e638dc9c6ffc1888c86896cc3fa874cb01
LeonardTai <89374421+LeonardTai@users.noreply.github.com>
2021-10-13 12:17:05 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -125,11 +125,11 @@ resource "aws_instance" "hashicat" {
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.hashicat.id
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
-  department                  = "devops"
-  billable                    = "true"
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
+    department                  = "devops"
+    billable                    = "true"
   }
 }
 
