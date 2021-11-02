6f15aa193549e2cedf3e11b12f8b6229b4edfd91
Bhupendra Kumar <bhupen.kumar@gmail.com>
2021-11-02 15:56:18 +0000

    added department tag

--- main.tf
+++ main.tf
@@ -125,10 +125,10 @@ resource "aws_instance" "hashicat" {
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.hashicat.id
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
-  department = "devops"
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
+    Department = "devops"
   }
 }
 
