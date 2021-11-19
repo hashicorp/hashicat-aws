53c4d535c449b7c0d56db4ebac444d30ae5cc5ea
naruoka <tak-naruoka@nec.com>
2021-11-19 05:25:16 +0000

    s3 001

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,8 +2,8 @@ module "s3-bucket" {
   source  = "app.terraform.io/tak-naruoka-training/s3-bucket/aws"
   version = "2.2.0"
   # insert required variables here
-  # hendana
-  bucket = "my-s3-bucket"
+  # henda henda
+  bucket = "naru-s3-bucket"
   acl    = "private"
 
   versioning = {
