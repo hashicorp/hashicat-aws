875fb38085c93241860aee3cec665fe3b96b2377
David Stanton <davidcstanton@hotmail.com>
2022-01-29 15:43:38 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -129,7 +129,7 @@ resource "aws_instance" "hashicat" {
   tags = {
     Name = "${var.prefix}-hashicat-instance"
     Department = "devops"
-    Billiable = "true"
+    Billable = "true"
     }
 }
 
