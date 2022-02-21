1963dcc0bbefbe9f849762fe361b5a629a7279a4
sheedalivings <100106244+sheedalivings@users.noreply.github.com>
2022-02-21 11:55:44 +0800

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,19 @@
-module "s3-bucket" {
+//--------------------------------------------------------------------
+// Variables
+variable "s3_bucket_acceleration_status" {}
+variable "s3_bucket_bucket" {}
+variable "s3_bucket_policy" {}
+variable "s3_bucket_request_payer" {}
+
+//--------------------------------------------------------------------
+// Modules
+module "s3_bucket" {
   source  = "app.terraform.io/alexlivings-training/s3-bucket/aws"
-  version = "2.8.0"
-  # insert required variables here
+  version = "2.14.1"
+
+  acceleration_status = "${var.s3_bucket_acceleration_status}"
+  bucket = "${var.s3_bucket_bucket}"
+  bucket_prefix = "alexlivings"
+  policy = "${var.s3_bucket_policy}"
+  request_payer = "${var.s3_bucket_request_payer}"
 }
