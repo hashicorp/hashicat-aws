7cd75500bb02e110b86fd4e1e0812861a99c3fb6
samuellee-zy <samuelee.zy@gmail.com>
2022-06-01 05:51:42 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,6 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
+module "s3-bucket" {
+  source  = "app.terraform.io/samuelleezy-hashicorp-demo/s3-bucket/aws"
+  version = "2.8.0"
+  # insert required variables here
+  bucket_prefix = "sam-lee"
 }
