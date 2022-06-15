f5a35813c7e3a9360447b2111b3170f5ef89d5b7
sakamossaaaan <sakamoto.akinori@teldevice.co.jp>
2022-06-15 07:03:59 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/sakamoto-training/s3-bucket/aws"
   version = "2.2.0"
-  bucket_prefix = "sakamoto akinori"
+  bucket_prefix = "${var.prefix}"
   # insert required variables here
 }
