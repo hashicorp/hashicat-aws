ffaea31ff859829048bf1a84a29a60f64436257a
Michelle Brennan <michelle.e.brennan@lmco.com>
2022-06-23 19:16:34 +0000

    Making cat comments

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  ":meow_lurk: Cats catch the best ice cream and popcorn_"
   <!-- END -->
 
   </div>
