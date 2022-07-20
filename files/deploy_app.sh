d78ba376fdc94ffab9e6901e325caab46de77e93
Joe Grenier <jwgrenier@gmail.com>
2022-07-20 15:44:22 -0400

    Fix typo

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. I have replaced the previos text with my own!
+  Welcome to ${PREFIX}'s app. I have replaced the previous text with my own!
   <!-- END -->
 
   </div>
