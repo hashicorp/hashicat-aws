092d8428a05a367ee1e5de2d4eb007b773e309b0
Rodrigo Torres <goncalvestorresrodrigo@gmail.com>
2022-07-20 19:26:02 +0000

    Bucket S3 com nome valido

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3-bucket" {
   source        = "app.terraform.io/rodrigogoncalvestorres/s3-bucket/aws"
   version       = "2.8.0"
-  bucket_prefix = var.prefix
+  bucket_prefix = "rodrigogoncalvestorres"
 }
