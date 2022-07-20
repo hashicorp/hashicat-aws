7f838a0f44ca09e5c23d7fd424522d683dd6ce24
Jeff Nelson <jeff.nelson@nelnet.net>
2022-07-20 11:53:32 -0500

    Updating with Department tags

--- main.tf
+++ main.tf
@@ -127,7 +127,7 @@ resource "aws_instance" "hashicat" {
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
 
   tags = {
-    Name = "${var.prefix}-hashicat-instance"
+    Department = "devops"
   }
 }
 
