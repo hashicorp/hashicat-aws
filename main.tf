b9462d8acb51fce214152ab85c4e223f10d9f365
Keller Giacomarro <keller.giacomarro@syf.com>
2022-08-10 16:29:38 +0000

    Resolve syntax error

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ provider "aws" {
   region  = var.region
 }
 
-resource "aws_vpcc" "hashicat" {
+resource "aws_vpc" "hashicat" {
   cidr_block           = var.address_space
   enable_dns_hostnames = true
 
