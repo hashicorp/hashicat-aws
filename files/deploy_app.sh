ec7f0a9b01a81f2ef9eb77a847cd8fab0299962d
songju <songju021@gmail.com>
2022-08-31 16:07:26 +0900

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. TEST TEST IWANT GO HOME.
+  Welcome to ${PREFIX}'s app. TEST TEST IWANT GO HOME_TEST_0831.
   <!-- END -->
 
   </div>
