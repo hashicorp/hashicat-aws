b946a66fc92ed72e98da88bad0d92b4a559bea76
Yogesh Mule <yogesh.mule@gmail.com>
2022-10-17 14:53:13 +0000

    Added remote backend

--- variables.tf
+++ variables.tf
@@ -6,6 +6,7 @@
 # prompted to enter it (or define it via config file or command line flags.)
 
 variable "prefix" {
+  default = "yes"
   description = "This prefix will be included in the name of most resources."
 }
 
