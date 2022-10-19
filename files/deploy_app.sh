e8713a55956cfc28291389233833aae96a2abe3c
yktakaah4 <20282867+yktakaha4@users.noreply.github.com>
2022-10-19 05:57:48 +0000

    add catch

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  あの ${PREFIX} のすきとおった風、夏でも底に冷たさをもつ青いそら
   <!-- END -->
 
   </div>
