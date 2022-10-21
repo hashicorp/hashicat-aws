ad5ab9c49eae0b2a7352dd720eb87345b6978e45
Boonyuen Sumphaongoen <boonyuen.sumphaongoen@krungsri.com>
2022-10-21 13:50:27 +0700

    changes welcome message.

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. meoaw meoaw, hello I am a pretty cat.
+  Welcome to ${PREFIX}'s app. Hello I am a pretty cat, meoaw meoaw.
   <!-- END -->
 
   </div>
