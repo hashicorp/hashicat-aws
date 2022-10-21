9b9177d6661800dff44d389589ac5d10e1511660
matayuuu <106732522+ggen-matayuuu@users.noreply.github.com>
2022-10-21 17:07:23 +0900

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,12 +11,13 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Welcome to matayuuu's app. Replace this text with your own.
   <!-- END -->
-
+  
   </div>
   </body>
 </html>
 EOM
 
 echo "Script complete."
+
