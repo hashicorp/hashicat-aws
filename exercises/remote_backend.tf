c329aa8171d3d7d74ebabf4145c95347229a2b7b
Chamika Diyunugalge <chamikadiyunugalge@pingidentity.com>
2022-11-09 19:06:52 +0000

    Added remote backend

--- exercises/remote_backend.tf
+++ exercises/remote_backend.tf
@@ -5,9 +5,9 @@
 terraform {
   backend "remote" {
     hostname = "app.terraform.io"
-    organization = "YOURORGANIZATION"
+    organization = "chamicorp"
     workspaces {
-      name = "YOURWORKSPACE"
+      name = "hashicat-aws"
     }
   }
 }
