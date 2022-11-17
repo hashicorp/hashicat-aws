ab59c8711378c03b88ee4cd0e9563a05142809df
Tom Warnock <warnock@gmail.com>
2022-11-17 22:38:52 +0000

    sg changes

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_security_group" "hashicat" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/8","172.0.0.0/8"]
   }
 
   ingress {
