a86edb70d6be6f877fc5432450cda9b1dbe8d63a
jason yu <condescendent@gmail.com>
2022-11-22 09:33:19 +0000

    one more

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
 module "s3-bucket" {
   source  = "app.terraform.io/TerraformOnAWS/s3-bucket/aws"
   version = "1.0.0"
-  bucket_prefix = jasonyujasontestbucket123432431433
+  bucket_prefix = "jasonyujasontestbucket123432431433"
 }
