f5e6db3f66c57e941fa2d0dab80f36b327d939c6
Jason Yu <condescendent@gmail.com>
2022-12-17 00:58:40 -0800

    added a new s3 bucket from module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,5 @@
-#module "s3-bucket" {
-#  source  = "app.terraform.io/TerraformOnAWS/s3-bucket/aws"
-#  version = "1.0.0"
-#  bucket_prefix = "jasonyujasontestbucket123432431433"
-#}
+module "s3-bucket" {
+  source  = "app.terraform.io/newterraformonawscloud/s3-bucket/aws"
+  version = "2.8.0"
+  bucket_prefix = "jason-yu"
+}
