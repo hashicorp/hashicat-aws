68c4cd5b455dff58f812644a8ce66bfc2a4cccea
Jason Yu <condescendent@gmail.com>
2022-12-17 00:33:55 -0800

    added tag

--- main.tf
+++ main.tf
@@ -128,7 +128,7 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Department = "devops"
+    Department = "devops-again"
     Billable = "true"
   }
 }
