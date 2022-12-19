569b8c7aa4ee56b6dbfeefed6f6aa0d254dadb72
Jason Yu <condescendent@gmail.com>
2022-12-19 00:47:21 -0800

    updated the ip address

--- main.tf
+++ main.tf
@@ -39,28 +39,28 @@ resource "aws_security_group" "hashicat" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["52.87.115.11/22"]
+    cidr_blocks = ["52.87.115.11/32"]
   }
 
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["52.87.115.11/22"]
+    cidr_blocks = ["52.87.115.11/32"]
   }
 
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = ["52.87.115.11/22"]
+    cidr_blocks = ["52.87.115.11/32"]
   }
 
   egress {
     from_port       = 0
     to_port         = 0
     protocol        = "-1"
-    cidr_blocks     = ["52.87.115.11/22"]
+    cidr_blocks     = ["52.87.115.11/32"]
     prefix_list_ids = []
   }
 
