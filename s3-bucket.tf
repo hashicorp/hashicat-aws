c6f7f76c9bbadbb6c890e17ac18e219e5f0ee16e
seullee-me <seul.lee.me@gmail.com>
2023-02-01 18:01:24 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,8 @@
 module "s3-bucket" {
   source  = "app.terraform.io/seul-training/s3-bucket/aws"
-  version = "3.6.1"
   bucket = "seul-s3-bucket"
   acl    = "private"
-
+  versioning = {
+    enabled = true
+  }
 }
