b5e5b9af6e79ed87027dccd0c03cb42da594df57
seullee-me <seul.lee.me@gmail.com>
2023-02-01 17:24:26 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -128,7 +128,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance",
-    Department = "devops"
+    Department = "devops",
+    Billable = true
   }
 }
 
