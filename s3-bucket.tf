694911acb8fd95a3acc928eb641ad267352b3a2d
Akarshi2602 <kapoorakarshi@gmail.com>
2023-02-22 06:14:47 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source  = "app.terraform.io/AnimalLogic26/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "my-s3-bucket"
+  bucket = "akarshikapoor"
   acl    = "private"
 
   versioning = {
