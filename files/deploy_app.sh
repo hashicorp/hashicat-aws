ddd36a018a241ff9448aea3b4b95e18af128d2f2
des35swim <derek_haskin2000@yahoo.com>
2023-02-22 02:58:56 +0000

    tesxt2

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Dereks catch of the day ok.
+  Welcome to ${PREFIX}'s app. Dereks catch of the day ok 2.
   <!-- END -->
 
   </div>
