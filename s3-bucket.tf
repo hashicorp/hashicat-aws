60e3520e4795fe07ed8904e70c291409488eae00
Akarshi2602 <kapoorakarshi@gmail.com>
2023-02-22 05:29:59 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,5 +1,7 @@
 module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
+  source  = "app.terraform.io/AnimalLogic26/s3-bucket/aws"
+  version = "2.8.0"
+}
 
   bucket = "my-s3-bucket"
   acl    = "private"
