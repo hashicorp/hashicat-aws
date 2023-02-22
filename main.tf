e4deffd23a84063e124a7c3f8ffd4634a12ce9a1
Akarshi2602 <kapoorakarshi@gmail.com>
2023-02-22 03:09:08 +0000

    Added remote backend

--- main.tf
+++ main.tf
@@ -197,3 +197,13 @@ resource "aws_key_pair" "hashicat" {
   key_name   = local.private_key_filename
   public_key = tls_private_key.hashicat.public_key_openssh
 }
+
+terraform {
+  cloud {
+    organization = "AnimalLogic26"
+
+    workspaces {
+      name = "hashicat-aws"
+    }
+  }
+}
---
+++ remote_backend.tf
@@ -0,0 +1,9 @@
+#terraform {
+#  backend "local" {
+#    hostname = "app.terraform.io"
+#    organization = "AnimalLogic26"
+#    workspaces {
+#      name = "hashicat-aws"
+#    }
+#  }
+#}
