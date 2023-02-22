b80aa41a214861b5017fc558e89bfb858d2fd8c2
Akarshi2602 <kapoorakarshi@gmail.com>
2023-02-22 05:24:56 +0000

    Added s3 bucket module

---
+++ s3-bucket.tf
@@ -0,0 +1,12 @@
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+  bucket_prefix = "akarshikapoor"
+
+  versioning = {
+    enabled = true
+  }
+
+}
