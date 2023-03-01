52e66a940f56cf19d9b0411c9dd8e4da9624c81b
Gopi Doli <gopi.1833@gmail.com>
2023-02-28 18:22:09 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,8 +7,18 @@ terraform {
   }
 }
 
+
 provider "aws" {
   region  = var.region
+}
+
+
+policy "enforce-mandatory-tags" {
+    enforcement_level = "hard-mandatory"
+}
+
+module "tfplan-functions" {
+  source = "https://raw.githubusercontent.com/hashicorp/terraform-sentinel-policies/main/common-functions/tfrun-functions/tfrun-functions.sentinel"
 }
 
 resource "aws_vpc" "hashicat" {
