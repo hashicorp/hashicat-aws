9106d01cbf4622018d7cabdff449a3ea0e045abf
Joe Thompson <github@orion-com.com>
2023-05-30 18:33:02 +0000

    Change marketing text

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  14-year-olds are nearly as obnoxious as 15-year-olds.
   <!-- END -->
 
   </div>
