336c81338963aeaec784018c6bea693dc9f3366e
root <naga2k19.aws@gmail.com>
2023-08-04 21:37:17 +0000

    Added remote backend

--- main.tf
+++ main.tf
@@ -197,3 +197,19 @@ resource "aws_key_pair" "hashicat" {
   key_name   = local.private_key_filename
   public_key = tls_private_key.hashicat.public_key_openssh
 }
+
+# data sources
+data "aws_caller_identity" "inuse" {}
+
+# outputs
+output "account_id" {
+  value = data.aws_caller_identity.inuse.account_id
+}
+
+output "caller_arn" {
+  value = data.aws_caller_identity.inuse.arn
+}
+
+output "caller_user" {
+  value = data.aws_caller_identity.inuse.user_id
+}
