6ba60a9297137a4cf5e27d5d8784eae6bc43ed0e
Nagarajan Ramanujam <naga2k19.aws@gmail.com>
2023-08-04 22:29:11 +0000

    Added the mandatory tags

--- main.tf
+++ main.tf
@@ -18,6 +18,8 @@ resource "aws_vpc" "hashicat" {
   tags = {
     name = "${var.prefix}-vpc-${var.region}"
     environment = "Production"
+    Department = "DevSecOps"
+    Billable = "Yes"
   }
 }
 
