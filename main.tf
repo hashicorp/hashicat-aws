6752a915244188ed96b5bf1b99faff91a9d24d9a
Romain Ségneré-Yter <romain.segnere-yter@devoteam.com>
2023-08-15 12:31:38 +0000

    Added the third tag

--- main.tf
+++ main.tf
@@ -129,7 +129,7 @@ resource "aws_instance" "hashicat" {
   tags = {
     Name        = "${var.prefix}-hashicat-instance"
     Departement = "devops"
-    Billable    = true
+    Billable    = "true"
   }
 }
 
