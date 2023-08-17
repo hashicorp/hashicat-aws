b789ff51f2b4f4a91876bdbedc8d804cb2575fd2
Matt Kurio <mkurio@amazon.com>
2023-08-17 15:59:30 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,9 +1,9 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
-
+  version = "2.8.0"
   bucket = "my-s3-bucket-kurio"
   acl    = "private"
-  #bucket_prefix = "${prefix}"
+  bucket_prefix = "${prefix}"
 
   control_object_ownership = true
   object_ownership         = "ObjectWriter"
