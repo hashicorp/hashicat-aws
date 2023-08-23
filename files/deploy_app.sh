eb66b132167a543d63e3fa4389525fa687997d72
prabhu <prabhu.ayyakkannu@gmail.com>
2023-08-23 21:03:38 +0000

    desc update

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. its aweSOME
+  Welcome to ${PREFIX}'s app. its aweSOME app
   <!-- END -->
 
   </div>
