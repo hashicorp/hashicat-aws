caac9e7d21ba54e0a9eeb775c329a0477bfee078
alaa abdelwahab <aladdin_h@hotmail.com>
2023-08-24 16:35:02 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -128,7 +128,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance",
-    Department = "aws_instance"
+    Department = "aws_instance",
+    Billable = "true"
   }
 }
 
