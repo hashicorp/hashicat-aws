0f238ce6f8df0af6f6b1543dabe9d31a31149ae7
vayu068 <vayu068@gmail.com>
2023-08-26 20:27:28 +0530

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  Welcome to Pawan's app. Brace Youself for Awesome Digital Experience.
   <!-- END -->
 
   </div>
