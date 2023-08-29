627fe755f408c2eb6590e8a1a1774919aed19677
Raj Rajkumar <91142733+rajv2021@users.noreply.github.com>
2023-08-29 13:28:50 -0500

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to TheModel2 app. Replace this text with your own.
+  Welcome to TheModel44 app. Replace this text with your own.
   <!-- END -->
 
   </div>
