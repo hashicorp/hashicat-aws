80735c0c59e78fdf1259fb0c478502ac858a0e62
HimoriShuhei <himori.shuhei@networld.co.jp>
2023-09-07 08:42:14 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,4 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
+module "s3-bucket" {
+  source  = "app.terraform.io/Trial_HS001/s3-bucket/aws"
+  version = "2.8.0"
 }
