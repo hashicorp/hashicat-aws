a00e932bac39838be7d40c1a89a6b662e2bf4263
praveendoifode <praveendoifode89@gmail.com>
2023-09-09 03:58:58 +0000

    version upgraded6

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket"
+  bucket = "praveendoifodeorg1989my-s3-bucket"
   acl    = "private"
 
   control_object_ownership = true
