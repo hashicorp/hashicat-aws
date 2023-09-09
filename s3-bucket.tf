4eb5b1b8bf189e72cd89faecab5d2c83ae774de5
praveendoifode <praveendoifode89@gmail.com>
2023-09-09 03:32:44 +0000

    added s3 code

---
+++ s3-bucket.tf
@@ -0,0 +1,13 @@
+module "s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+}
