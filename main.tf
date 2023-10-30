f2fc5d2efef33e3970c7670fee7c081304f59354
Paula marie <paulamrieabracosa@delteke.com>
2023-10-30 12:43:38 +0000

    Added the first tag

--- main.tf
+++ main.tf
@@ -125,6 +125,7 @@ resource "aws_instance" "hashicat" {
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.hashicat.id
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
+  department                  = devops
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
