7ab0c09a5dcfa6afb373d768a8e2553230e92fa5
paulamarieabracosa <148229852+paulamarieabracosa@users.noreply.github.com>
2023-10-30 21:13:13 +0800

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,3 +1,5 @@
+//--------------------------------------------------------------------
+// Modules
 module "s3_bucket" {
   source  = "app.terraform.io/pola-deltek-session2/s3-bucket/aws"
   version = "2.8.0"
