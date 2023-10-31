82b345d90b95efd66b43d8042c267f5afda09222
Rodolphe Fontaine <30529563+Rodi26@users.noreply.github.com>
2023-10-31 11:43:01 +0100

    First commit

---
+++ exercises/outputs.tf.completed
@@ -0,0 +1,8 @@
+# Outputs file
+output "catapp_url" {
+  value = "http://${aws_eip.hashicat.public_dns}"
+}
+
+output "catapp_ip" {
+  value = "http://${aws_eip.hashicat.public_ip}"
+}
---
+++ main.tf
@@ -0,0 +1,128 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "=3.42.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.region
+}
+
+resource "aws_vpc" "hashicat" {
+  cidr_block           = var.address_space
+  enable_dns_hostnames = true
+
+  tags = {
+    name = "${var.prefix}-vpc-${var.region}"
+    environment = "Production"
+  }
+}
+
+resource "aws_subnet" "hashicat" {
+  vpc_id     = aws_vpc.hashicat.id
+  cidr_block = var.subnet_prefix
+
+  tags = {
+    name = "${var.prefix}-subnet"
+  }
+}
+
+resource "aws_security_group" "hashicat" {
+  name = "${var.prefix}-security-group"
+
+  vpc_id = aws_vpc.hashicat.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "${var.prefix}-security-group"
+  }
+}
+
+resource "aws_internet_gateway" "hashicat" {
+  vpc_id = aws_vpc.hashicat.id
+
+  tags = {
+    Name = "${var.prefix}-internet-gateway"
+  }
+}
+
+resource "aws_route_table" "hashicat" {
+  vpc_id = aws_vpc.hashicat.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.hashicat.id
+  }
+}
+
+resource "aws_route_table_association" "hashicat" {
+  subnet_id      = aws_subnet.hashicat.id
+  route_table_id = aws_route_table.hashicat.id
+}
+
+
+
+resource "aws_eip" "hashicat" {
+  instance = aws_instance.hashicat.id
+  vpc      = true
+}
+
+resource "aws_eip_association" "hashicat" {
+  instance_id   = aws_instance.hashicat.id
+  allocation_id = aws_eip.hashicat.id
+}
+
+data "hcp_packer_image" "hashicat-aws" {
+  bucket_name     = "hashicat-aws"
+  channel         = "latest"
+  cloud_provider  = "aws"
+  region          = "us-east-1"
+}
+
+# Then replace your existing references with
+# data.hcp_packer_image.hashicat-aws.cloud_image_id
+
+resource "aws_instance" "hashicat" {
+  ami                         = data.hcp_packer_image.hashicat-aws.cloud_image_id
+  instance_type               = var.instance_type
+  associate_public_ip_address = true
+  subnet_id                   = aws_subnet.hashicat.id
+  vpc_security_group_ids      = [aws_security_group.hashicat.id]
+
+  tags = {
+    Name = "${var.prefix}-hashicat-instance"
+  }
+}
+
+
