a2c93952ccbc897e7cf7c8041673aff98e014d70
Rodolphe Fontaine <30529563+Rodi26@users.noreply.github.com>
2023-10-31 10:51:58 +0100

    add packer

--- main.tf
+++ main.tf
@@ -91,22 +91,6 @@ resource "aws_route_table_association" "hashicat" {
   route_table_id = aws_route_table.hashicat.id
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name = "name"
-    #values = ["ubuntu/images/hvm-ssd/ubuntu-disco-19.04-amd64-server-*"]
-    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
 
 resource "aws_eip" "hashicat" {
   instance = aws_instance.hashicat.id
@@ -118,10 +102,17 @@ resource "aws_eip_association" "hashicat" {
   allocation_id = aws_eip.hashicat.id
 }
 
+data "hcp_packer_image" "hashicat-aws" {
+  bucket_name     = "hashicat-aws"
+  channel         = "latest"
+  cloud_provider  = "aws"
+  region          = "us-east-1"
+}
+
+
 resource "aws_instance" "hashicat" {
-  ami                         = data.aws_ami.ubuntu.id
+  ami                         = data.hcp_packer_image.hashicat-aws.cloud_image_id
   instance_type               = var.instance_type
-  key_name                    = aws_key_pair.hashicat.key_name
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.hashicat.id
   vpc_security_group_ids      = [aws_security_group.hashicat.id]
@@ -131,69 +122,5 @@ resource "aws_instance" "hashicat" {
   }
 }
 
-# We're using a little trick here so we can run the provisioner without
-# destroying the VM. Do not do this in production.
 
-# If you need ongoing management (Day N) of your virtual machines a tool such
-# as Chef or Puppet is a better choice. These tools track the state of
-# individual files and can keep them in the correct configuration.
 
-# Here we do the following steps:
-# Sync everything in files/ to the remote VM.
-# Set up some environment variables for our script.
-# Add execute permissions to our scripts.
-# Run the deploy_app.sh script.
-resource "null_resource" "configure-cat-app" {
-  depends_on = [aws_eip_association.hashicat]
-
-  triggers = {
-    build_number = timestamp()
-  }
-
-  provisioner "file" {
-    source      = "files/"
-    destination = "/home/ubuntu/"
-
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = tls_private_key.hashicat.private_key_pem
-      host        = aws_eip.hashicat.public_ip
-    }
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt -y update",
-      "sleep 15",
-      "sudo apt -y update",
-      "sudo apt -y install apache2",
-      "sudo systemctl start apache2",
-      "sudo chown -R ubuntu:ubuntu /var/www/html",
-      "chmod +x *.sh",
-      "PLACEHOLDER=${var.placeholder} WIDTH=${var.width} HEIGHT=${var.height} PREFIX=${var.prefix} ./deploy_app.sh",
-      "sudo apt -y install cowsay",
-      "cowsay Mooooooooooo!",
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = tls_private_key.hashicat.private_key_pem
-      host        = aws_eip.hashicat.public_ip
-    }
-  }
-}
-
-resource "tls_private_key" "hashicat" {
-  algorithm = "ED25519"
-}
-
-locals {
-  private_key_filename = "${var.prefix}-ssh-key.pem"
-}
-
-resource "aws_key_pair" "hashicat" {
-  key_name   = local.private_key_filename
-  public_key = tls_private_key.hashicat.public_key_openssh
-}
