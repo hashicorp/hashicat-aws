a0cee2ddd4b87236bd547cd98a50bb92cd29a4db
hedykim <waltzbox@gmail.com>
2023-12-10 02:19:42 +0000

    Added remote backend

--- variables.tf
+++ variables.tf
@@ -26,7 +26,7 @@ variable "subnet_prefix" {
 
 variable "instance_type" {
   description = "Specifies the AWS instance type."
-  default     = "t2.micro"
+  default     = "t3.micro"
 }
 
 variable "admin_username" {
