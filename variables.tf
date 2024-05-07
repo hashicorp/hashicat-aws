207f63bcc45e5b079fe9930812f5514f798788ef
egarelnabi <eibashir@yahoo.com>
2024-05-07 12:17:05 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -48,3 +48,8 @@ variable "placeholder" {
   default     = "placekitten.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
+
+variable "environment" {
+  default    = "Production"
+  description = "target environment"
+}
