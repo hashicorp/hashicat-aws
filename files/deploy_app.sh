c271d4c3e51a6d565841de4bdedf7b12f0db28fe
sreenathprem <sreenath.premnadh@hashicorp.com>
2024-06-09 10:03:56 +0200

    Changing the Messages

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -10,8 +10,8 @@ cat << EOM > /var/www/html/index.html
 
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Replace this text with your own.
+  <center><h2>Hello World!</h2></center>
+  Welcome to ${PREFIX}'s app.
   <!-- END -->
 
   </div>
