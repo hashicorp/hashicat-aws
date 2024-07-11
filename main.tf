fbe36dc5c40c5d813bd39daacd66babd3656db6c
sreenathprem <sreenath.premnadh@hashicorp.com>
2024-07-11 14:52:20 +0200

    Little changes

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "vpc" {
 
   env = "dev"
   public_subnet_cidr_blocks = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  vpc_cidr_block = "10.0.0.0/16"
+  vpc_cidr_block = "10.1.200.0/24"
 }
 
 resource "aws_vpc" "hashicat" {
