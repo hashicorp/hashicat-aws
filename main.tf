3651a4cdedfcb759ad80f88cb3a3289acddc7195
Ryan Pecor <123400049+rpecor-sonar@users.noreply.github.com>
2025-04-04 14:58:31 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -197,3 +197,8 @@ resource "aws_key_pair" "hashicat" {
   key_name   = local.private_key_filename
   public_key = tls_private_key.hashicat.public_key_openssh
 }
+
+resource "aws_dms_replication_instance" "example" {
+  publicly_accessible = true # Sensitive
+
+}
